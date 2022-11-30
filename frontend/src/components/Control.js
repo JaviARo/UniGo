08e25d3f48a1c3e5240ee48ac46695468aab4026
JaviARo <javieralemanrodriguez@alumno.ieselrincon.es>
2022-11-30 19:03:50 +0000

    fourth commit

--- frontend/src/components/Control.js
+++ frontend/src/components/Control.js
@@ -1,18 +1,26 @@
-import './control.css'
+import { Col, Row } from "antd";
+import "./control.css";
 
 export function Header() {
   return (
     <div id="header">
-      <img class="icon" src="/img/info.png" alt=""/>
-      <img class="icon" src="/img/config.png" alt=""/>
+      <Row align="middle">
+        <Col className="row" span={6}>
+          <img class="icon" src="/img/info.png" alt="" />
+        </Col>
+        <Col className="row" span={12}></Col>
+        <Col className="row" span={6}>
+          <img class="icon" src="/img/config.png" alt="" />
+        </Col>
+      </Row>
     </div>
-  )
+  );
 }
 
 export function Footer() {
   return (
     <div id="footer">
-
+      <img class="icon" src="/img/logo1.png" alt="" />
     </div>
-  )
+  );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Header, Footer } from "../components/Control";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
